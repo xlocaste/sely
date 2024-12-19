b57381bbe200cbf6fab6ecf44ba1e0a1fb732c81
xlocaste <irfandi.di122@gmail.com>
2024-12-19 11:34:11 +0700

    fix: padding x responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import PrimaryButton from "./button"
 
 const Navbar = () => {
     return (
-        <div className="fixed flex justify-between w-full px-20 py-4">
+        <div className="fixed flex justify-between w-full px-4 md:px-20 py-4">
             <div className="flex items-center">
                 <img src="/logo.png" alt="" className="w-10"/>
             </div>
